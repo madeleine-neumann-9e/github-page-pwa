515b0597e3aa1e239a0bdec690451e3c81372e08
Madeleine Neumann <madeleine.neumann@9elements.com>
2022-04-23 15:31:31 +0200

    another try

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('I am running!');
+function calculate(e) {
+  e.preventDefault();
+  const widthFormOne = document.getElementById("widthOne").value;
+  const lengthFormOne = document.getElementById("lengthOne").value;
+
+  const widthFormTwo = document.getElementById("widthTwo").value;
+  const lengthFormTwo = document.getElementById("lengthTwo").value;
+  const panHeight = 1;
+
+  let sizePanA = parseFloat(widthFormOne) * parseFloat(lengthFormOne) * panHeight
+  let sizePanB = parseFloat(widthFormTwo) * parseFloat(lengthFormTwo) * panHeight
+
+  document.getElementById('ingredientMultiply').innerHTML = `<span>${(sizePanB / sizePanA).toFixed(1)}</span>`;
+}
